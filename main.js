34fc86df4bb4a5390f67ea5c1dbe42d32d5303ba
ZaidMarrie <zaidmarrie27@gmail.com>
2021-12-02 16:04:58 +0200

    second attempt initial commit

--- main.js
+++
@@ -1,6 +0,0 @@
-let menuItems = document.querySelector('.navbar');
-let menuButton = document.querySelector('#menuIcon');
-
-menuButton.addEventListener('click', () => {
-    menuItems.classList.toggle('show');
-});
