83de76c5535abe8f0ac445dd4c4e3b6e876fdd06
ZaidMarrie <zaidmarrie27@gmail.com>
2021-12-27 14:25:33 +0200

    update .gitignore and add prod build

---
+++ dist/script.js
@@ -0,0 +1,2 @@
+"use strict";var menuToggle=document.querySelector(".hamburger"),menu=document.querySelector(".nav__list");function toggleMenu(){menu.classList.contains("active")?menu.classList.remove("active"):menu.classList.add("active")}menuToggle.addEventListener("click",toggleMenu);
+//# sourceMappingURL=script.js.map
